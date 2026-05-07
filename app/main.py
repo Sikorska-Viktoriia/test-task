4388eb68c977f69d663d78b3283f8b79ac5822fa
Sikorska Viktoriia <sikorska.viktoriia@gmail.com>
2026-05-07 18:05:01 +0300

    improve project structure and initialization setup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
-
-from .database import engine
-from .models import Base
-
-from .routers import projects
-from .routers import places
+from .database import engine, Base
+from .routers import projects, places
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title="Travel Planner API"
-)
+app = FastAPI(title="Travel Planner API")
 
 app.include_router(projects.router)
 app.include_router(places.router)
